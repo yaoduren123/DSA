5e27602384e671373f47b6847a99b67ce25825de
yaoduren123 <chinaren1314@gmail.com>
2015-04-29 08:05:32 +0800

    modify stack

--- stack/index.js
+++ stack/index.js
@@ -23,6 +23,7 @@ Stack.prototype.clear = function(){
   this.stop = 0;
 }
 
+//数制间相互转换
 function mulBase(num, base) {
   var s = new Stack();
   do {
@@ -34,4 +35,29 @@ function mulBase(num, base) {
      converted += s.pop();
   }
   return converted;
+}
+
+//()匹配
+function braceMatch(str){
+  var stack = new Stack(),
+      i = -1,
+      rword = '',
+      braceMap = {
+        '(' : ')',
+        ')' : '('
+      },
+      len = str.length;
+  while(++i <= len){
+    if(str[i]=='('){
+      stack.push(i)
+    }else if(str[i]==')'){
+      typeof stack.pop() == 'undefined' && console.log(i+':not matched')
+
+    }
+    if(stack.length()){
+      console.log(stack.pop() + ': not matched');
+    }
+  }
+
+
 }
